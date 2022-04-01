269990f9c4b201f399253e1b28a991dd773bf3ce
Urfeh <keiicristobal32@gmail.com>
2022-04-02 04:36:09 +0800

    email

--- src/clients/screens/ContactUs.jsx
+++ src/clients/screens/ContactUs.jsx
@@ -2,8 +2,47 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import ContactUsCSS from '../css/ContactUs.module.css';
+import Swal from 'sweetalert2'
 
-function ContactUs(){
+const ContactUs = () => {
+
+const [status, setStatus] = React.useState("Submit");
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+    setStatus("Sending...");
+    const { fname, lname, email, msg } = e.target.elements;
+    let details = {
+      fname: fname.value,
+      lname: lname.value,
+      email: email.value,
+      msg: msg.value,
+    };
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    //alert(result.status);
+    Swal.fire({
+        title: 'Message sent!',
+        text: 'Thank you for getting in touch with us.',
+        icon: 'success',
+        timer:2000,
+        showConfirmButton: false,
+        customClass:{
+            icon: 'swalertIcon'
+        }
+    }).then((response) => {
+    window.location.reload();
+    })
+  };
+    
     return(
         <div className = {ContactUsCSS.container}>
             <Header/>
@@ -11,27 +50,27 @@ function ContactUs(){
                     <h1 className={ContactUsCSS.contactTopHeading}>GET IN TOUCH WITH US</h1>
                     <p className={ContactUsCSS.contactTopText}>You can also email us at email@gmail.com</p>
                 </div>
-                <form className={ContactUsCSS.formGroup}>
+                <form className={ContactUsCSS.formGroup} onSubmit={handleSubmit}>
                     <div className={ContactUsCSS.formGroup1}>
                         <div className={ContactUsCSS.formGroup1Content}>
                             FIRST NAME
-                            <input type="text" name="fname"></input>
+                            <input type="text" name="fname" id="fname" required></input>
                         </div>
                         <div className={ContactUsCSS.formGroup1Content}>
                             LAST NAME
-                            <input type="text" name="lname"></input>
+                            <input type="text" name="lname" id="lname" required></input>
                         </div>
                     </div>
                     <div className={ContactUsCSS.formGroup2}>
                         EMAIL ADDRESS
-                        <input type = "text" name="email"></input>
+                        <input type = "text" name="email" id="email" required></input>
                     </div>
                     <div className={ContactUsCSS.formGroup3}>
                         MESSAGE
-                        <input type = "text" name="msg"></input>
+                        <input type = "text" name="msg" id="msg" required></input>
                     </div>
-                    <button className={ContactUsCSS.formButton}>
-                        RESERVE NOW
+                    <button className={ContactUsCSS.formButton} type="submit">
+                        {status}
                     </button>
                 </form>
             <Footer/>
