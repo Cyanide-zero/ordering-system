9da3c037046665964e30b1945a5fbe962d8fe902
Cyanide-zero <doteiy001@gmail.com>
2022-04-01 10:14:04 +0800

    Edit Products

--- src/admins/screens/ProductManagement.jsx
+++ src/admins/screens/ProductManagement.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 // import ProductTable from '../components/product-table';
 import axios from 'axios';
 import Modal from 'react-modal'
+import Swal from 'sweetalert2';
 
 function ProductManagement(){
     const [arr,setArr] = React.useState([]);
@@ -13,6 +14,7 @@ function ProductManagement(){
     const [folder, setFolder] = React.useState("");
     const [newName, setNewName] = React.useState("");
     const [newPrice, setNewPrice] = React.useState(0);
+    const [date, setDate] = React.useState("")
 
   React.useEffect(() => {
     getDrinks();
@@ -20,10 +22,11 @@ function ProductManagement(){
     setNewName(localStorage.getItem("product"));
     setFolder(localStorage.getItem("folder"));
     setID(localStorage.getItem('id'));
+    setDate(localStorage.getItem('date'))
     console.log(`api/admin/update${folder}`)
   }, [openModal]);
 
-  const getDrinks = () =>{
+  const getDrinks = () =>{  
       axios.get("https://ordering-system-database.herokuapp.com/api/admin/getproduct")
           .then((response) => {
                 setArr(response.data);
@@ -32,13 +35,39 @@ function ProductManagement(){
 
   const updateProduct = () => {
       console.log(newName, newPrice, folder);
-      axios.put(`https://ordering-system-database.herokuapp.com/api/admin/update${folder}`,{
+      if((parseInt(localStorage.getItem("price")) == newPrice) && (localStorage.getItem("product") == newName)){
+        Swal.fire({
+            title: 'Product Edit Failed',
+            text: 'No changes were made',
+            icon: 'warning',
+            confirmButtonText: 'Try Again',
+            customClass:{
+                icon: 'swalertIcon'
+            }
+        })
+      }else{
+        Swal.fire({
+            title: 'Edit Successful',
+            text: 'Changes were made successfully',
+            icon: 'success',
+            confirmButtonText: 'OK',
+            customClass:{
+                icon: 'swalertIcon'
+            }
+        })
+        axios.put(`https://ordering-system-database.herokuapp.com/api/admin/update${folder}`,{
           id : id,
           price: newPrice,
           name: newName
-      }).then((response)=>{
-        console.log(response)
-    })
+        }).then((response)=>{
+            setArr(arr.map((val)=>{
+                return val.id == id? {id: id, menuName: newName, folder: folder, price: newPrice, date: date} : val
+            }))
+            console.log(response)
+        })
+        setOpenModal(false)
+      }
+      
   }
 
     return(
@@ -79,34 +108,43 @@ function ProductManagement(){
                                                 position:'absolute',
                                                 top:'20vh',
                                                 left:'25vw',
+                                                display:'flex',
+                                                flexDirection:'column',
+                                                alignItems:'center',
+                                                justifyContent:"space-evenly"
                                             }
                                         }} isOpen={openModal} onRequestClose={()=>setOpenModal(false)}>
                                             <h1 className='modalTitle'>Edit Product</h1>
-                                            <p>New Product Name</p>
+                                            <p className='modalP'>New Product Name</p>
                                             <input
+                                                className='modalInput'
                                                 type="text"
                                                 name="newname"
                                                 onChange={(e)=>setNewName(e.target.value)}
                                             /><br></br>
-                                            <p>New Price</p>
+                                            <p className='modalP'>New Price</p>
                                             <input
+                                                className='modalInput'
                                                 type="number"
                                                 name="newname"
                                                 onChange={(e)=>setNewPrice(e.target.value)}
                                             /><br></br>
-                                            <button onClick={updateProduct}>Submit</button>
+                                            <button className='modalSubmitButton' onClick={updateProduct}>Submit</button>
                                         </Modal>
                                         <td className='admintd'>{index+1}</td>
                                         <td className='admintd'>{item.menuName}</td>
                                         <td className='admintd'>{item.price}</td>
-                                        <td className='admintd'></td>
-                                        <td className='admintd'><button onClick={()=>{
+                                        <td className='admintd'>{item.date}</td>
+                                        <td className='admintd'><button className='editProductButton' onClick={()=>{
                                             setOpenModal(true);
                                             localStorage.setItem("product", item.menuName);
                                             localStorage.setItem('price', item.price);
                                             localStorage.setItem('folder', item.folder);
                                             localStorage.setItem('id', item.id);
-                                        }}> Edit </button>  &nbsp;|&nbsp;   <button> Delete </button></td>
+                                            localStorage.setItem('date',item.date);
+                                        }}> ✒ </button>  
+                                        {/* &nbsp;|&nbsp;   <button> Delete </button> */}
+                                        </td>
                                     </tr>
                                 )
                                })
