399131b6ca3c4b2c3a2c633c5a9046901e86fd1c
Cyanide-zero <doteiy001@gmail.com>
2022-04-01 08:51:25 +0800

    Trial Edit Product

--- src/admins/screens/ProductManagement.jsx
+++ src/admins/screens/ProductManagement.jsx
@@ -4,19 +4,41 @@ import '../css/AdminIndent.css';
 import { Link } from "react-router-dom";
 // import ProductTable from '../components/product-table';
 import axios from 'axios';
+import Modal from 'react-modal'
 
 function ProductManagement(){
     const [arr,setArr] = React.useState([]);
+    const [openModal, setOpenModal] = React.useState(false);
+    const [id, setID] = React.useState(0);
+    const [folder, setFolder] = React.useState("");
+    const [newName, setNewName] = React.useState("");
+    const [newPrice, setNewPrice] = React.useState(0);
 
   React.useEffect(() => {
     getDrinks();
-  }, []);
+    setNewPrice(localStorage.getItem("price"));
+    setNewName(localStorage.getItem("product"));
+    setFolder(localStorage.getItem("folder"));
+    setID(localStorage.getItem('id'));
+    console.log(`api/admin/update${folder}`)
+  }, [openModal]);
 
   const getDrinks = () =>{
       axios.get("https://ordering-system-database.herokuapp.com/api/admin/getproduct")
           .then((response) => {
                 setArr(response.data);
       });
+  }
+
+  const updateProduct = () => {
+      console.log(newName, newPrice, folder);
+      axios.put(`https://ordering-system-database.herokuapp.com/api/admin/update${folder}`,{
+          id : id,
+          price: newPrice,
+          name: newName
+      }).then((response)=>{
+        console.log(response)
+    })
   }
 
     return(
@@ -47,11 +69,44 @@ function ProductManagement(){
                             arr.map((item,index) =>{
                                 return(
                                     <tr>
+                                        <Modal style={{
+                                            overlay:{
+                                                backgroundColor:'rgba(0, 0, 0, 0.02)'
+                                            },
+                                            content:{
+                                                height:'50vh',
+                                                width:'50vw',
+                                                position:'absolute',
+                                                top:'20vh',
+                                                left:'25vw',
+                                            }
+                                        }} isOpen={openModal} onRequestClose={()=>setOpenModal(false)}>
+                                            <h1 className='modalTitle'>Edit Product</h1>
+                                            <p>New Product Name</p>
+                                            <input
+                                                type="text"
+                                                name="newname"
+                                                onChange={(e)=>setNewName(e.target.value)}
+                                            /><br></br>
+                                            <p>New Price</p>
+                                            <input
+                                                type="number"
+                                                name="newname"
+                                                onChange={(e)=>setNewPrice(e.target.value)}
+                                            /><br></br>
+                                            <button onClick={updateProduct}>Submit</button>
+                                        </Modal>
                                         <td className='admintd'>{index+1}</td>
                                         <td className='admintd'>{item.menuName}</td>
                                         <td className='admintd'>{item.price}</td>
                                         <td className='admintd'></td>
-                                        <td className='admintd'><button> Edit </button>  &nbsp;|&nbsp;   <button> Delete </button></td>
+                                        <td className='admintd'><button onClick={()=>{
+                                            setOpenModal(true);
+                                            localStorage.setItem("product", item.menuName);
+                                            localStorage.setItem('price', item.price);
+                                            localStorage.setItem('folder', item.folder);
+                                            localStorage.setItem('id', item.id);
+                                        }}> Edit </button>  &nbsp;|&nbsp;   <button> Delete </button></td>
                                     </tr>
                                 )
                                })
