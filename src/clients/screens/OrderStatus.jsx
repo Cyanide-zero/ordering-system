c1faf21366977dd0242a05a8ec4395e4dcaae10a
Cyanide-zero <doteiy001@gmail.com>
2022-04-11 20:36:11 +0800

    Reload

--- src/clients/screens/OrderStatus.jsx
+++ src/clients/screens/OrderStatus.jsx
@@ -47,6 +47,8 @@ function OrderStatus(){
                             customClass:{
                                 icon:OrderStatusCSS.swalertIcon
                             }
+                        }).then((response)=>{
+                            window.location.reload();
                         })
                 })
             }
